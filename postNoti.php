76e2fbe9326344f4233a8cf46b1d43f8660c56b9
EGM\luis.castro <kstrolap@gmail.com>
2021-07-08 18:19:01 -0500

    url de notificacion2

--- postNoti.php
+++ postNoti.php
@@ -1,21 +1,17 @@
 <?php
 
 $data = file_get_contents('php://input');
-echo $data;
-
-
-
-$elJson= $data;
+//echo $data;
 
 $url = 'https://afix.proyectosgulupa.com/wp-json/placetopay-payment/v2/callback/';
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $elJson);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=UTF-8', 'User-Agent: cUrl Testing'));
 $result = curl_exec($ch);
 $respuestaJson = json_decode($result);
 
-echo $respuestaJson; 
+echo $respuestaJson;
 
  ?>
