195dbfc6994432afc1796ba9689d5a9c49f6e7da
EGM\luis.castro <kstrolap@gmail.com>
2021-07-09 09:22:41 -0500

    url de para C#

--- postNoti.php
+++ postNoti.php
@@ -3,12 +3,12 @@
 $data = file_get_contents('php://input');
 //echo $data;
 
-$url = 'https://afix.proyectosgulupa.com/wp-json/placetopay-payment/v2/callback/';
+$url = 'https://www.almacenesunidos.cr/Plugins/PaymentP2P/IPNHandler';
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=UTF-8', 'User-Agent: cUrl Testing'));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=UTF-8'));
 $result = curl_exec($ch);
 $respuestaJson = json_decode($result);
 
